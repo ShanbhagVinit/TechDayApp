4e345c7d8cbd16691e6af3b5f53e767b65512746
Vinit Shanbhag <Vinit.Shanbagh@in.tesco.com>
2016-08-23 13:17:47 +0530

    Initial app stage

---
+++ TecdDayApp/TecdDayApp/StallsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  StallsViewController.swift
+//  TecdDayApp
+//
+//  Created by Tesco on 8/22/16.
+//  Copyright © 2016 Tecso. All rights reserved.
+//
+
+import UIKit
+
+class StallsViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationItem.title = "TESCO"
+        //self.navigationController?.title =  "Tesco"
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
